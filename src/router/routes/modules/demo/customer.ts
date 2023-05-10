1cd0add40248cb176f52781ebcee9c471ccb188c
yongchao.liang <yongchao.liang@upuphone.com>
2023-05-10 18:24:18 +0800

    feat: 项目配置调整

---
+++ src/router/routes/modules/demo/customer.ts
@@ -0,0 +1,70 @@
+import type { AppRouteModule } from '/@/router/types';
+
+import { LAYOUT } from '/@/router/constant';
+import { t } from '/@/hooks/web/useI18n';
+
+const customer: AppRouteModule = {
+  path: '/customer',
+  name: 'Customer',
+  component: LAYOUT,
+  redirect: '/feat/icon',
+  meta: {
+    orderNo: 19,
+    icon: 'ion:git-compare-outline',
+    title: t('routes.demo.customer.init'),
+  },
+  children: [
+    {
+      path: 'search',
+      name: 'Search',
+      component: () => import('/@/views/demo/feat/icon/index.vue'),
+      meta: {
+        title: t('routes.demo.customer.search'),
+      },
+    },
+    {
+      path: 'info',
+      name: 'Info',
+      component: () => import('/@/views/demo/feat/ws/index.vue'),
+      meta: {
+        title: t('routes.demo.customer.info'),
+      },
+    },
+    {
+      path: 'mHistory',
+      name: 'MHistory',
+      // @ts-ignore
+      component: () => import('/@/views/demo/feat/request-demo/index.vue'),
+      meta: {
+        title: t('routes.demo.customer.mHistory'),
+      },
+    },
+    {
+      path: 'order',
+      name: 'Order',
+      component: () => import('/@/views/demo/feat/session-timeout/index.vue'),
+      meta: {
+        title: t('routes.demo.customer.order'),
+      },
+    },
+    {
+      path: 'contact',
+      name: 'Contact',
+      component: () => import('/@/views/demo/feat/print/index.vue'),
+      meta: {
+        title: t('routes.demo.customer.contact'),
+      },
+    },
+    {
+      path: 'invoice',
+      name: 'Invoice',
+      component: () => import('/@/views/demo/feat/tabs/index.vue'),
+      meta: {
+        title: t('routes.demo.customer.invoice'),
+        hideChildrenInMenu: true,
+      },
+    },
+  ],
+};
+
+export default customer;
---
+++ src/router/routes/modules/demo/return-visit.ts
@@ -0,0 +1,69 @@
+import type { AppRouteModule } from '/@/router/types';
+
+import { LAYOUT } from '/@/router/constant';
+import { t } from '/@/hooks/web/useI18n';
+
+const returnVisit: AppRouteModule = {
+  path: '/return-visit',
+  name: 'ReturnVisit',
+  component: LAYOUT,
+  redirect: '/return-visit/search',
+  meta: {
+    orderNo: 20,
+    icon: 'ion:aperture-outline',
+    title: t('routes.demo.return-visit.index'),
+  },
+  children: [
+    {
+      path: 'search',
+      name: 'ReturnVisitSearch',
+      component: () => import('/@/views/demo/page/form/basic/index.vue'),
+      meta: {
+        title: t('routes.demo.return-visit.search'),
+      },
+    },
+    {
+      path: 'add',
+      name: 'ReturnVisitAdd',
+      component: () => import('/@/views/demo/page/form/step/index.vue'),
+      meta: {
+        title: t('routes.demo.return-visit.add'),
+      },
+    },
+    {
+      path: 'edit',
+      name: 'ReturnVisitEdit',
+      component: () => import('/@/views/demo/page/form/high/index.vue'),
+      meta: {
+        title: t('routes.demo.return-visit.edit'),
+      },
+    },
+    {
+      path: 'begin',
+      name: 'ReturnVisitBegin',
+      component: () => import('/@/views/demo/page/desc/high/index.vue'),
+      meta: {
+        title: t('routes.demo.return-visit.begin'),
+      },
+    },
+    {
+      path: 'cancel',
+      name: 'ReturnVisitCancel',
+      component: () => import('/@/views/demo/page/form/high/index.vue'),
+      meta: {
+        title: t('routes.demo.return-visit.cancel'),
+      },
+    },
+
+    {
+      path: 'print',
+      name: 'DescBasicPage',
+      component: () => import('/@/views/demo/page/desc/basic/index.vue'),
+      meta: {
+        title: t('routes.demo.return-visit.print'),
+      },
+    },
+  ],
+};
+
+export default returnVisit;
